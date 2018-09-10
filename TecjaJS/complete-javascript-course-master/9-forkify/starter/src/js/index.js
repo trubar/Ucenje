1e984b4501e2dfc005eba070253a0aaae7b1c260
Andraž Bečaj <draza34@gmail.com>
2018-09-10 23:16:27 +0200

    render ajax loading spinner

--- TecjaJS/complete-javascript-course-master/9-forkify/starter/src/js/index.js
+++ TecjaJS/complete-javascript-course-master/9-forkify/starter/src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /**
  * Global stanje aplikacije
@@ -22,11 +22,14 @@ const controlSearch = async () => {
     // 3. Prepare UI for results
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
+
 
     // 4. Search for recipies
     await state.search.getResults(); // ker je await moram zgoraj funkcijo narediti async
 
     // 5. Render results on UI
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 }
