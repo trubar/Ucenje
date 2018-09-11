1258133a6c4735ceed54825f1fb7e504748df747
Andraž Bečaj <draza34@gmail.com>
2018-09-11 10:43:37 +0200

    search končan

--- TecjaJS/complete-javascript-course-master/9-forkify/starter/src/js/views/searchView.js
+++ TecjaJS/complete-javascript-course-master/9-forkify/starter/src/js/views/searchView.js
@@ -5,9 +5,10 @@ export const getInput = () => elements.searchInput.value;
 export const clearInput = () => {
     elements.searchInput.value = '';
 };
-
+ 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 }
 
 // 'pasta with tomato and spinach'
@@ -50,6 +51,44 @@ const renderRecipe = recipe => {
     elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+// type: prev or next
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page -1 : page + 1}>
+        <span>Page ${type === 'prev' ? page -1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+
+    let button;
+    if (page === 1 && pages > 1){
+        // samo gumb za naprej
+        button = createButton(page, 'next');
+    } else if (page < pages) {
+        // oba gumba
+        button = `
+        ${createButton(page, 'next')}
+        ${createButton(page, 'prev')}
+        `;
+    } else if (page === pages && pages > 1) {
+        // samo gumb za nazaj
+        button = createButton(page, 'prev');
+    }
+
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    // render zadetke za trenutno stran
+    const start = (page -1) * resPerPage;
+    const end = page * resPerPage;
+
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    // render gumbe za strani
+    renderButtons(page, recipes.length, resPerPage);
 };
