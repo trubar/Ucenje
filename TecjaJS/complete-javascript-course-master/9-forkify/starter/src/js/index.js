0cb666b5c66a7b39e839ac9666736bf158ea14cd
Andraž Bečaj <draza34@gmail.com>
2018-09-18 12:29:24 +0200

    Building recipe view part 1

--- TecjaJS/complete-javascript-course-master/9-forkify/starter/src/js/index.js
+++ TecjaJS/complete-javascript-course-master/9-forkify/starter/src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import {
   elements,
   renderLoader,
@@ -70,6 +71,8 @@ const controlRecipe = async () => {
 
   if (id) {
     // Pripravi UI na spremembe
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
 
     // Ustvari objekt recept
     state.recipe = new Recipe(id); //id dobim zgoraj iz url, nato ustvarim objekt iz Recipe.js
@@ -84,7 +87,10 @@ const controlRecipe = async () => {
       state.recipe.calcServings();
   
       // Izpiši recept
-      console.log(state.recipe);
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+
+
     } catch (err) {
       console.log(err);
       alert('Ne morem izpisati recepta brez podatkov!');
